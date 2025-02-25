15c5ec3bd621dd3da6d2aa6ce739ffcadf655a09
asrithalingala <alingala@hawk.iit.edu>
2025-02-24 19:16:51 -0600

    Setup Express server with authentication routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ dotenv.config()
 console.log("MONGODB_URI:", process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+app.use(express.json())
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, ()=>{
