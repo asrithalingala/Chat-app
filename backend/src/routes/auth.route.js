9547b6ac60fe8f44105b6ff910f056c3ed4d2879
asrithalingala <alingala@hawk.iit.edu>
2025-02-24 21:13:19 -0600

    Added PUT route for updating profile picture with authentication using protectRoute middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express"
-import {signup, login,logout } from "../controllers/auth.controller.js";
+import {signup, login,logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup",signup)
@@ -7,5 +9,6 @@ router.post("/signup",signup)
 router.post("/login", login)
 
 router.post("/logout", logout)
+router.put("/update-profile", protectRoute, updateProfile)
 
 export default router;
