8d11a11eccff4e3a07adfeeb8b1d57357ac26ecd
asrithalingala <alingala@hawk.iit.edu>
2025-02-24 21:16:09 -0600

    setup express server with cookie parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
+import cookieParser from "cookie-parser"
 import dotenv from 'dotenv';
 import {connectDB}from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
+
 dotenv.config()
-console.log("MONGODB_URI:", process.env.MONGO_URI);
 const app = express();
+console.log("MONGODB_URI:", process.env.MONGO_URI);
+
 const PORT = process.env.PORT || 5001;
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, ()=>{
