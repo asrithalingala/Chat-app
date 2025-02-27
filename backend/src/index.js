e0aae2c72d815bd4abc25d7165e0ee8e441c23eb
asrithalingala <alingala@hawk.iit.edu>
2025-02-26 23:25:25 -0600

    modified

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser"
 import dotenv from 'dotenv';
 import {connectDB}from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
+app.use("/api/messgae", messageRoutes)
 
 app.listen(PORT, ()=>{
     console.log("server started successfully on PORT:"+ PORT);
