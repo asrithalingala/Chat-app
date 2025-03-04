a2e046641efbab30cfee8b61805e29f1e5f05c5f
asrithalingala <alingala@hawk.iit.edu>
2025-03-04 17:48:55 -0600

    Set up server with routes, socket integration, and static file serving for production"

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,42 @@
 import express from "express";
-import cookieParser from "cookie-parser"
-import dotenv from 'dotenv';
-import {connectDB}from "./lib/db.js"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-dotenv.config()
-const app = express();
-console.log("MONGODB_URI:", process.env.MONGO_URI);
+import path from "path";
 
-const PORT = process.env.PORT || 5001;
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth", authRoutes)
-app.use("/api/messgae", messageRoutes)
+import { connectDB } from "./lib/db.js";
 
-app.listen(PORT, ()=>{
-    console.log("server started successfully on PORT:"+ PORT);
-    
-})
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-connectDB();
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
